a65dec9f672fb550e1b90e79b887248f186b41e0
marcopeakdev <peakgenius226@gmail.com>
2024-11-20 19:45:17 -0700

    updated header for the improment of the app

---
+++ components/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Content,
+  Button,
+  Text,
+  Icon,
+  View
+} from "native-base";
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { StyleSheet, TouchableHighlight, Image } from "react-native";
+class header extends Component {
+  render() {
+    return (
+      <header>
+        <Text> Hello World !!!</Text>
+      </header>
+    );
+  }
+}
+
+export default header;
